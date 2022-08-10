8b1ea3ed0b8427993464d5faca11ef406572b767
haileyport <sugar9935@gmail.com>
2022-08-10 16:20:05 +0900

    feat: main 페이지 1차 마무리

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 
 const Main = () => {
-  const [text, setText] = useState("");
   const css =
     "rounded-md focus:outline-none focus:border-sky-500 focus:bg-slate-100 focus:ring-sky-500 focus:ring-1 text-gray-800 ";
+  const [text, setText] = useState("");
   const [newCSs, setNewCss] = useState(css);
   const data = {
     hello: "안녕하세요",
@@ -25,13 +25,14 @@ const Main = () => {
   };
   return (
     <div className="h-full">
-      <main className="left-1/2 top-1/4	rounded-lg border-2 border-yellow-50">
-        <form>
+      <main className="flex items-center">
+        <form className="mx-32 px-9 py-9 left-1/2 top-1/4	rounded-lg border-2 border-yellow-50">
           <span>빈칸 채우기 for 40th 스터디 카페</span>
           <span>빈칸채우기 하는법</span>
           <br />
           <br />
           <span className="text-gray-800">
+            {/* TODO: 정답인 경우 focus bg 변경 */}
             <input
               id="hello"
               type="text"
