313e5fe80709db97fa1a79b1e6a1ee8adddc54af
haileyport <sugar9935@gmail.com>
2022-08-13 15:57:21 +0900

    build: 페이지 주소 재설정

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <div>
         <Menu />
         <div className="pt-36 h-auto">
